d4d5b8b5a35d7884220213302533712a2d449154
Jeff Tarr <jeff@tarr.com>
2016-01-02 12:25:41 -0500

    my updates

---
+++ aliases/available/custom.aliases.bash
@@ -0,0 +1,12 @@
+# Lock the screen (when going AFK)
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+# Reload the shell (i.e. invoke as a login shell)
+alias reload="exec $SHELL -l"
+
+# Make less behave with termcap
+alias less="/usr/bin/less -F -X"
+
+alias j="jobs"
+alias drop="cd ~/TarrDropbox"
+alias dropa="cd ~/TarrDropbox/Articulated\ Tech/Development/"
--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Path to the bash it configuration
-export BASH_IT="{{BASH_IT}}"
+export BASH_IT='/Users/jefftarr/TarrDropbox/Computer_Setup/bash-it'
 
 # Lock and Load a custom theme file
 # location /.bash_it/themes/
@@ -26,5 +26,51 @@ export SCM_CHECK=true
 # https://github.com/xvzf/vcprompt
 #export VCPROMPT_EXECUTABLE=~/.vcprompt/bin/vcprompt
 
+#PATH updates
+export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
+export PATH="$HOME/.rvm/bin:$PATH"
+export PATH="/opt/local/bin:$PATH"
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.3/bin
+export PATH="/usr/local/bin:$PATH"
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
+
+# Highlight section titles in manual pages
+export LESS_TERMCAP_md="$ORANGE"
+
+# Don’t clear the screen after quitting a manual page
+export MANPAGER="less -X"
+
+# Always enable colored `grep` output
+export GREP_OPTIONS="--color=auto"
+
+# Case-insensitive globbing (used in pathname expansion)
+shopt -s nocaseglob
+
+# Append to the Bash history file, rather than overwriting it
+shopt -s histappend
+
+# Autocorrect typos in path names when using `cd`
+shopt -s cdspell
+
+# Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
+[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2 | tr ' ' '\n')" scp sftp ssh
+
+git config --global user.name "Jeff Tarr"
+git config --global user.email jeff@tarr.com
+git config --global push.default simple
+
+export NODE_ENV="dev"
+export NVM_DIR="/Users/jefftarr/.nvm"
+source ~/.nvm/nvm.sh
+nvm use 4
+
 # Load Bash It
 source $BASH_IT/bash_it.sh
+cp ~/.bash_profile $BASH_IT/template/bash_profile.template.bash
+
+bash-it enable alias custom > /dev/null
+bash-it enable alias atom > /dev/null
+bash-it enable alias general > /dev/null
+uptime
